80325caf2a65b966f05fc3664ca9f7b53c83425c
Яна Буга <yana.buga129@gmail.com>
2023-05-30 22:23:01 +0300

    Initial Commit

---
+++ CatsAndModules_YanaBuha/CatsAndModules_YanaBuhaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsAndModules_YanaBuhaApp.swift
+//  CatsAndModules_YanaBuha
+//
+//  Created by Yana Buha on 30.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct CatsAndModules_YanaBuhaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
